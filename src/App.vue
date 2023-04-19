19fe10e62e94f54524f9a7431d97aba7ba263a68
Niozk <99679240+Niozk@users.noreply.github.com>
2023-04-19 16:01:40 +0200

    Weather app FINAL 1.0
    -Alles werkt
    -Edge cases getest
    -Volledig responsive
    -Animaties toegevoegd

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-    <Header />
-    <Nav />
+    <Header class="intro"/>
+    <Nav class="intro"/>
     <WeatherBox />
     <ErrorPopup />
 </template>
@@ -10,6 +10,22 @@ import Header from './components/Header.vue'
 import Nav from './components/Nav.vue'
 import WeatherBox from './components/WeatherBox.vue'
 import ErrorPopup from './components/ErrorPopup.vue'
+import { onMounted } from 'vue';
+import { animate, stagger, spring } from "motion"
+
+let viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+let initialScale = (viewportWidth <= 600) ? 1 : 2;
+
+onMounted(() => {
+    animate(
+        ".intro",
+        { scale: [initialScale, 1] },
+        { 
+            easing: spring(),
+            delay: stagger(0.1)
+        }
+    )
+});
 </script>
 
 <style scoped>
